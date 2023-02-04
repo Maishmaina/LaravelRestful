893863e6ba9f569ca88ccc1dadba92ad78185d28
Maishmaina <simpledaniel.1818@gmail.com>
2023-02-04 10:44:44 +0300

    Laravel Restful intro

---
+++ app/Model/Sclass.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Sclass extends Model
+{
+    
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'class_name'
+    ];
+}
