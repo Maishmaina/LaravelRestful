2d1be19fd3341e8a04c843cbb813d851039656b3
Maishmaina <simpledaniel.1818@gmail.com>
2023-02-09 19:29:03 +0300

    updated and started jwt token

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use DB;
+use App\Model\Student;
 
 class StudentController extends Controller
 {
@@ -14,6 +15,7 @@ class StudentController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
     public function index()
     {
         $student=DB::table('students')->get();
@@ -48,9 +50,10 @@ class StudentController extends Controller
      */
     public function show($id)
     {
-        
+      //$student=DB::table('students')->where('id',$id)->first();
+      $student= Student::findOrFail($id); 
+      return response()->json($student);
     }
-
     /**
      * Update the specified resource in storage.
      *
@@ -60,9 +63,19 @@ class StudentController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $data=array();
+        $data['class_id']= $request->class_id;
+        $data['section_id']= $request->section_id;
+        $data['name']= $request->name;
+        $data['phone']= $request->phone;
+        $data['email']= $request->email;
+        $data['password']= Hash::make($request->password);
+        $data['photo']= $request->photo;
+        $data['address']= $request->address;
+        $data['gender']= $request->gender;
+        DB::table('students')->where('id',$id)->update($data);
+        return response('Student Updated');
     }
-
     /**
      * Remove the specified resource from storage.
      *
@@ -71,6 +84,10 @@ class StudentController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $img = DB::table('students')->where('id',$id)->first();
+        $img_path=$img->photo;
+        unlink($img_path);
+        DB::table('students')->where('id',$id)->delete();
+        return response('Student Deleted');        
     }
 }
