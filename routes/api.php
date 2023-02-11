b2b366e1aa58dcb9d04916e9bddf0d975bc1e6af
Maishmaina <simpledaniel.1818@gmail.com>
2023-02-11 10:10:54 +0300

    jwt auth for api

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,14 @@ Route::ApiResource('/class','Api\SclassController');
 Route::ApiResource('/subject','Api\SubjectController');
 Route::ApiResource('/section','Api\SectionController');
 Route::ApiResource('/student','Api\StudentController');
+
+Route::group([
+
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
